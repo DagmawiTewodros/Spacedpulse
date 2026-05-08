8823c79228b5daee328fe1aec13a0b9f343decc7
dagmawi <dagipommy@gmail.com>
2026-05-08 17:51:13 +0300

    Adjust main.dart to have routing

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 import 'core/routing/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FarmKeeperApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FarmKeeperApp extends StatelessWidget {
+  const FarmKeeperApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'FarmKeeper',
       debugShowCheckedModeBanner: false,
+
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2E7D32)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF2E7D32),
+        ),
         useMaterial3: true,
         fontFamily: 'Roboto',
       ),
+
       routerConfig: appRouter,
     );
   }
