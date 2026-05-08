aa19538c0004a6d0a9d07e01f13491f4b8b0fc9c
dagmawi <dagipommy@gmail.com>
2026-05-08 17:34:44 +0300

    Add routing to the pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,19 @@
-import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/notifications_screen.dart';
-import 'screens/profile_screen.dart';
-import 'screens/settings_screen.dart';
+import 'package:go_router/go_router.dart';
+import '../../screens/splash_screen.dart';
+import '../../screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/home_screen',
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const SplashScreen(),
+    ),
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FarmKeeper',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF2E7D32),
-        ),
-        useMaterial3: true,
-        fontFamily: 'Roboto',
-      ),
-
-      initialRoute: '/home',
-
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/home': (context) => const HomeScreen(),
-
-        // Added Screens
-        '/notifications': (context) => const NotificationsPage(),
-        '/profile': (context) => const ProfilePage(),
-        '/settings': (context) => const SettingsPage(),
-      },
-    );
-  }
-}
+    GoRoute(
+      path: '/home_screen',
+      builder: (context, state) => const HomeScreen(),
+    ),
+  ],
+);
